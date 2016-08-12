668bf47cd0edf7d065bc16419c3c5b4d36d1b3a1
20metura01 <serzhant1990@mail.ru>
2016-08-12 21:23:45 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -4,19 +4,14 @@ public class Calculate {
 		int one = Integer.valueOf(arg[0]);
 		int two = Integer.valueOf(arg[1]);
 		int sum = one + two;
+		int sub = one - two;
+		int mult = one * two;
+		double div = (double)one / two;
 		System.out.println("Summ " + sum);
-		int three = Integer.valueOf(arg[0]);
-		int four = Integer.valueOf(arg[1]);
-		int sub = three - four;
 		System.out.println("Sub " + sub);
-		int five = Integer.valueOf(arg[0]);
-		int six = Integer.valueOf(arg[1]);
-		int mult = five * six;
 		System.out.println("Mult " + mult);
-		double seven = Integer.valueOf(arg[0]);
-		int ache = Integer.valueOf(arg[1]);
-		double div = seven / ache;
 		System.out.println("Div " + div);
+		
 		
 		
 	}

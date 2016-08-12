e0abc2d110b94d994390ba443f600de9c5764f53
20metura01 <serzhant1990@mail.ru>
2016-08-12 19:20:25 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,15 @@
 public class Calculate {
 	public static void main(String[] arg){
 		System.out.println("Calculate..");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sum = first + second;
+		System.out.println("Summ " + sum);
+		int tw = Integer.valueOf(arg[0]);
+		int tr = Integer.valueOf(arg[1]);
+		int sub = tw - tr;
+		System.out.println("Sub " + sub);
+		
 	}
 	
 }
